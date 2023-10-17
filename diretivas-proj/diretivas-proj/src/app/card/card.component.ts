30e3d4b560b9340fd1b59f0c96540d29032beae9
Roniselton <ronibarreto@gmail.com>
2023-10-16 23:32:01 -0300

    Diretivas NgModel

--- diretivas-proj/diretivas-proj/src/app/card/card.component.ts
+++ diretivas-proj/diretivas-proj/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+    produtos:string[] = [];
+
+    @Input() novoItem:string ="";
+
+    menuType:string = "user";
+
+    consultarProdutos():void{
+      this.produtos = ["Melao","Laranja","Limao","Maca"];
+    }
+
+    adicionar():void{
+      this.produtos.push( this.novoItem );
+      this.novoItem = "";
+    }
+
+    remover(index:number){
+
+      this.produtos.splice( index , index );
+      // let item = delete this.produtos[index]; //remove o elemento mas não retira o espaço
+    }
 
 }
---
+++ diretivas-proj/diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-atributos',
+  templateUrl: './comp-atributos.component.html',
+  styleUrls: ['./comp-atributos.component.css']
+})
+export class CompAtributosComponent {
+  estilo:string = "enabled";
+  corFundo:string = "#DDDDDD";
+}
